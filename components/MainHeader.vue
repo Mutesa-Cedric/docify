f2b5d9c502fccb9d4ab476755dab628bb80e0fa2
Mutesa-Cedric <mutesacedric@gmail.com>
2022-07-31 13:19:14 +0200

    feat : algolia search

--- components/MainHeader.vue
+++ components/MainHeader.vue
@@ -1,61 +1,38 @@
 <template>
-  <div
-    class="
-      fixed
-      z-10
-      flex flex-shrink-0
-      h-16
-      w-full
-      max-w-6xl
-      items-center
-      justify-between
-      bg-white
-      border-b-2 border-gray-100
-    "
-  >
+  <div class="fixed z-10 flex flex-shrink-0 h-16 top-0 w-full max-w-6xl items-center justify-between bg-white border-b-2 border-gray-100">
+
     <!-- Mobile menu button -->
     <button class="md:hidden items-center px-4" @click="showMobileMenu">
-      <svg
-        class="h-6 w-6"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M4 6h16M4 12h16M4 18h16"
-        />
+      <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
       </svg>
     </button>
 
     <div class="hidden md:flex px-4">
-      <nuxt-link to="/">
-        <img src="https://www.insperity.com/wp-content/uploads/document_HR__1200x600.png" class="h-10 w-10 rounded-full" />
-      </nuxt-link>
+      <!-- Swap out the logo with one of your own -->
+      <a href="/">
+        <logo class="h-8 w-8"/>
+      </a>
     </div>
 
     <!-- search -->
     <Search class="border-solid border-2 border-gray-400" />
 
-  <GlobeIcon/>
+    <!-- Swap out the logo with one of your own - can be a link to your main site, a social icon, app login, etc. -->
+    <a href="https://cleavr.io" class="px-4">
+      <GlobeIcon class="mr-1 h-5 w-5 text-gray-700 hover:text-gray-800"  />
+    </a>
   </div>
 </template>
 
 <script>
-import Search from './Search.vue'
-import GlobeIcon from './GlobeIcon.vue';
+import GlobeIcon from "~/components/GlobeIcon";
 export default {
-    components: { Search, GlobeIcon },
-    methods: {
+  components: {GlobeIcon},
+  methods: {
     showMobileMenu () {
       this.$emit('showMobileMenu') // closes the mobile menu if in open state and when route changes
     }
   }
-};
+}
 </script>
-
-<style>
-</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,32 +1,33 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'docify',
+    title: "docify",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: 'https://www.insperity.com/wp-content/uploads/document_HR__1200x600.png' }
-    ]
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "https://www.insperity.com/wp-content/uploads/document_HR__1200x600.png",
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-
-  ],
+  plugins: ["~/plugins/vue-instantsearch"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -34,15 +35,35 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss",
+    "nuxt-content-algolia",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    '@nuxt/content'
-  ],
+  modules: ["@nuxt/content"],
+
+  nuxtContentAlgolia: {
+    appId: process.env.APP_ID,
+    apiKey: process.env.PRIVATE_API_KEY,
+    paths: [
+      {
+        name: "documentation",
+        index: "doc",
+        fields: ["title", "description", "bodyPlainText"],
+      },
+    ],
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
-}
+    transpile: ["vue-instantsearch", "instantsearch.js/es"],
+  },
+  hooks: {
+    "content:file:beforeInsert": (document) => {
+      const removeMd = require("remove-markdown");
+      if (document.extension === "md") {
+        document.bodyPlainText = removeMd(document.text);
+      }
+    },
+  },
+};
